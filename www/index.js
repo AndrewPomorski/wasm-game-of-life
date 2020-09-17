c1a26b234be1329468e22eae5d4e8e7f2decb339
andrew <andrew@shepherdcmms.com>
2020-09-17 21:29:36 +0800

    code cleanup 🧹

--- www/index.js
+++ www/index.js
@@ -1,68 +1,38 @@
-import { Universe, Cell } from "wasm-game-of-life";
+import { Universe, Cell } from 'wasm-game-of-life';
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
 
 const CELL_SIZE = 5;
 const GRID_COLOR = '#CCCCCC';
 const DEAD_COLOR = '#FFFFFF';
 const ALIVE_COLOR = '#FF0000';
-const universe = Universe.new();
-const width = universe.width(); 
-const height = universe.height();
-
 let FPS = 30;
 
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
 
+const fpsSlider = document.getElementById('fps-slider');
 const pausePlayBtn = document.getElementById('play-pause');
 pausePlayBtn.textContent = '⏯︎';
-const canvas = document.getElementById("game-of-life-canvas");
+
+const canvas = document.getElementById('game-of-life-canvas');
 const ctx = canvas.getContext('2d');
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 let animationFrame = null;
 
-const renderLoop = () => {
-  setTimeout(() => {
-
-    universe.tick();
-    
-    drawGrid();
-    drawCells();
-
-    animationFrame = requestAnimationFrame(renderLoop);
-  }, 1000 / FPS);
-};
-
-const isPaused = () => {
-  return animationFrame === null;
-}
-
-
-const play = () => {
-  renderLoop();
-};
-
-const pause = () => {
-  cancelAnimationFrame(animationFrame);
-  animationFrame= null;
-};
-
-pausePlayBtn.addEventListener("click", event => {
-  if (isPaused()) {
-    play();
-  } else {
-    pause();
-  }
-});
+const isPaused = () => animationFrame === null;
+const getIndex = (row, column) => row * width + column;
 
 const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
 
   // Vertical lines
-  for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * (CELL_SIZE +1) + 1, 0);
-    ctx.lineTo(i * (CELL_SIZE +1) + 1, (CELL_SIZE + 1) * height + 1);
+  for (let i = 0; i <= width; i++ ) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
   for (let j = 0; j <= height; j++ ) {
@@ -73,14 +43,12 @@ const drawGrid = () => {
   ctx.stroke();
 };
 
-const getIndex = (row, column) => row * width + column;
-
 const drawCells = () => {
   const cellsPtr = universe.cells();
   const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
   ctx.beginPath();
-  
+
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
@@ -93,7 +61,7 @@ const drawCells = () => {
         col * (CELL_SIZE + 1) + 1,
         row * (CELL_SIZE + 1) + 1,
         CELL_SIZE,
-        CELL_SIZE
+        CELL_SIZE,
       );
     }
   }
@@ -101,9 +69,55 @@ const drawCells = () => {
   ctx.stroke();
 };
 
+canvas.addEventListener('click', (event) => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+});
+
+const renderLoop = () => {
+  setTimeout(() => {
+    universe.tick();
+    drawGrid();
+    drawCells();
+    
+    animationFrame = requestAnimationFrame(renderLoop);
+
+  }, 1000 / FPS);
+};
+
+const play = () => {
+  renderLoop();
+};
+
+const pause = () => {
+  cancelAnimationFrame(animationFrame);
+  animationFrame = null;
+};
+pausePlayBtn.addEventListener('click', () => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+fpsSlider.addEventListener('change', (event) => {
+  FPS = event.currentTarget.value;
+});
+
 drawGrid();
 drawCells();
 play();
-
-document.getElementById('fps-slider').addEventListener('change', e => FPS = e.currentTarget.value); 
-
